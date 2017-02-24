b5350f03f3482cf622aa21011e2d4db4fa2530fe
lKanade <Kanade@outlook.es>
2017-02-23 19:00:08 -0600

    Ajustes
    Mejorado la vista en consola de distacia y aumentada la distancia minima
    en 50cm

--- CodigoFuente/ProgramaArduino.ino/ProgramaArduino.ino.ino
+++ CodigoFuente/ProgramaArduino.ino/ProgramaArduino.ino.ino
@@ -11,25 +11,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Sensor();
+  Sensor(); 
 }
+
 void Sensor(){
   //sensor ultrasonico
   
   digitalWrite(9,LOW);
   delayMicroseconds(5);
   digitalWrite(9, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(5);
   tiempo = pulseIn(8, HIGH);
-  dist = int (0.017*tiempo);
-  Serial.println("Distancia: ");
-  Serial.println(dist);
-  Serial.println(" cm");
+  dist = int (0.017*tiempo); // REVISAR LA FORMULA (CREO QUE SE TIENE QUE DIVIR ENTRE 2)
+  Serial.print("Distancia: ");
+  Serial.print(dist);
+  Serial.println(" cm/hr");
   delay(1000);
-  if(dist < 20){
+  if(dist < 50){
     digitalWrite(13,HIGH);
   } else{
     digitalWrite(13,LOW);
   }
 }
-
+// FUNCIONALIDAD DE LOS MOTORES
