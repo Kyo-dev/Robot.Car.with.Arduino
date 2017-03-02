693dd3f0eb91fb3c8d340c9dc78276812bd79358
lKanade <Kanade@outlook.es>
2017-03-02 11:46:04 -0600

    Movimiento
    Funciona rueda derecha (acelera y reversa)

--- CodigoFuente/ProgramaArduino.ino/ProgramaArduino/ProgramaArduino.ino
+++ CodigoFuente/ProgramaArduino.ino/ProgramaArduino/ProgramaArduino.ino
@@ -1,5 +1,7 @@
 long dist;
 long tiempo;
+int IN3 = 5; 
+int IN4 = 4;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,14 +9,16 @@ void setup() {
   pinMode(8,INPUT);
   pinMode(9, OUTPUT);
   pinMode (13,OUTPUT);
+  pinMode (IN4, OUTPUT);    // Input4 conectada al pin 4 
+  pinMode (IN3, OUTPUT); // Input3 conectada al pin 5
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Run();
 }
 
 void Sensor(){ // No esta instanciado en loop
+  
   //sensor ultrasonico
   
   digitalWrite(9,LOW);
@@ -34,5 +38,17 @@ void Sensor(){ // No esta instanciado en loop
   }
 }
 // FUNCIONALIDAD DE LOS MOTORES
-
-
+void Run (){
+  digitalWrite (IN4, HIGH);
+  digitalWrite (IN3, LOW); 
+  delay(4000);
+  // Motor no gira
+  digitalWrite (IN4, LOW); 
+  delay(500);
+  // Motor gira en sentido inverso
+  digitalWrite (IN3, HIGH);
+  delay(4000);
+  // Motor no gira
+  digitalWrite (IN3, LOW); 
+delay(5000);
+}
