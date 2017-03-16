60377fa22a08d8f9c969bec0c7fb537a8d5e0a92
lKanade <Kanade@outlook.es>
2017-03-16 00:16:42 -0600

    Corregino metodo del sensor de proximidad

--- CodigoFuente/ProgramaArduino.ino
+++ CodigoFuente/ProgramaArduino.ino
@@ -5,8 +5,18 @@ int Contor_In2 = 6;
 // CONTROL DEL MOTOR IZQUIERDO
 int Contor_In3 = 3;
 int Contor_In4 = 4;
+// CONTROL DEL MOTOR DERECHO
+int Contor_In1 = 7;
+int Contor_In2 = 6;
+
+// CONTROL DEL MOTOR IZQUIERDO
+int Contor_In3 = 3;
+int Contor_In4 = 4;
 
 // VARIABLES PARA CONTROLAR EL SENSOR ULTRASONICO O DE PROXIMIDAD
+long dist;
+long time;
+int ledPrueba = 13;
 
 void setup() {
   // PIN DE LOS MOTORES
@@ -17,23 +27,24 @@ void setup() {
 
   // PIN PARA EL SENSOR DE PROXIMIDAD
   Serial.begin(9600);
-  pinMode(9,INPUT); //9 ENTRADA DE DATOS ECO
-  pinMode(8, OUTPUT); //10 SALIDA DE DATOS
-  pinMode (13,OUTPUT); // LED
+  pinMode(9,INPUT); //9 ENTRADA DE DATOS ECCO
+  pinMode(8, OUTPUT); //10 SALIDA DE DATOS TRIGGER
+  pinMode (13,OUTPUT); // LED DEL PIN 13
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Run();
+  // Run();
+  Sensor();
 }
 // FUNCIONALIDAD DEL SENSOR
 void Sensor(){ // No esta instanciado en looP
-  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
   delayMicroseconds(5);
-  digitalWrite(9, HIGH);
-  delayMicroseconds(5);
-  tiempo = pulseIn(8, HIGH);
-  dist = int (0.017*tiempo); // REVISAR LA FORMULA (CREO QUE SE TIENE QUE DIVIR ENTRE 2)
+  digitalWrite(8, HIGH);
+  delayMicroseconds(10);
+  time = pulseIn(9, HIGH);
+  dist = (0.017*time); // REVISAR LA FORMULA (CREO QUE SE TIENE QUE DIVIR ENTRE 2)
   Serial.print("Distancia: ");
   Serial.print(dist);
   Serial.println(" cm/hr");
@@ -60,7 +71,7 @@ void Stop(){
   digitalWrite(Contor_In4, LOW);
 }
 
-void Right{
+void Right(){
   digitalWrite(Contor_In1, LOW);
   digitalWrite(Contor_In2, HIGH);
   digitalWrite(Contor_In3, HIGH);
